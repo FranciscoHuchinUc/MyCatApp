31230f14d7d8adfc6c7441b17487e425b7306b2c
Francisco Huchin <huchin.uc.francisco@hotmail.com>
2022-02-11 09:01:33 -0600

    add Link Action

--- js/app.js
+++ js/app.js
@@ -85,3 +85,13 @@ function getRandomArbitrary(min, max) {
 }
 
 getListBreeds()
+
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show-menu')
+    navClose.classList.remove('show-close')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
